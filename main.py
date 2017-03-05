c8916e1f24dd8348ae99b5188e395524d33b3b7c
bellatoris <bellatoris@snu.ac.kr>
2017-03-05 21:51:17 +0900

    [PoseNet] Res-Pose

--- main.py
+++ main.py
@@ -23,6 +23,17 @@ def main():
 
     cudnn.benchmark = True
 
+    # Data loading code
+    traindir = os.path.join('./dataset', 'train')
+
+    train_loader = torch.utils.data.DataLoader(
+        datasets.ImageFolder(traindir, transforms.Compose([
+            transforms.RandomCrop(224),
+            transforms.ToTensor()
+        ])),
+        batch_szize=75, num_workers=4, pin_memory=True
+    )
 
 if __name__ == '__main__':
+
     main()
